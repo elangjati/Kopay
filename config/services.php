50eb8be056e6ce0400636569096565f7c43a1ffe
elang <elangjati38@gmail.com>
2026-05-14 21:31:22 +0700

    chore: hapus konfigurasi Midtrans dari env.example dan services.php

--- config/services.php
+++ config/services.php
@@ -39,10 +39,4 @@ return [
         'spreadsheet_id' => env('GOOGLE_SHEETS_ID'),
     ],
 
-    'midtrans' => [
-        'server_key'    => env('MIDTRANS_SERVER_KEY', ''),
-        'client_key'    => env('MIDTRANS_CLIENT_KEY', ''),
-        'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
-    ],
-
 ];
